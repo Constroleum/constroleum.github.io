1941662e9ec9f4f2ad10f8a8f2b9c50cac11a1bb
Constroleum <114590162+Constroleum@users.noreply.github.com>
2025-01-17 09:02:07 +0100

    Now
    This reverts commit c6637dafaff539c4497c323b3ca7e6a42f865432.

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const video = document.getElementById('hero-video');
+    const hamburger = document.querySelector('.hamburger');
+    const navMenu = document.querySelector('.nav-menu');
 
-    // Si deseas controlar el video de alguna manera (pausar, reproducir, etc.),
-    // puedes añadir eventos como estos:
-
-    // Por ejemplo, poner en pausa cuando el usuario hace clic en el video
+    // Control del video
     video.addEventListener('click', () => {
         if (video.paused) {
             video.play();
         } else {
             video.pause();
         }
+    });
+
+    // Control del menú hamburguesa
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('open');
+        hamburger.classList.toggle('active');
     });
 });
