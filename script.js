9e98e1172d37b5214f039f1102080dbc74277009
Constroleum <114590162+Constroleum@users.noreply.github.com>
2025-01-20 08:38:47 +0100

    main

--- script.js
+++ script.js
@@ -32,4 +32,32 @@ document.addEventListener('DOMContentLoaded', () => {
     // Asignar el nombre del archivo al título
     item.querySelector(".title").textContent = fileName.replace(/_/g, " "); // Reemplazar guiones bajos con espacios
   });
+
+  // Obtén los elementos de la galería y el modal
+const galleryItems = document.querySelectorAll('.item');
+const modal = document.getElementById('modal');
+const modalImg = document.getElementById('modalImg');
+const closeBtn = document.getElementById('closeBtn');
+
+// Abre la ventana modal con la imagen clickeada
+galleryItems.forEach(item => {
+  item.addEventListener('click', () => {
+    const imageUrl = item.getAttribute('data-image');
+    modal.style.display = 'block';
+    modalImg.src = imageUrl;
+  });
+});
+
+// Cierra la ventana modal al hacer clic en el botón de cerrar
+closeBtn.addEventListener('click', () => {
+  modal.style.display = 'none';
+});
+
+// Cierra la ventana modal si el usuario hace clic fuera de la imagen
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
 })
